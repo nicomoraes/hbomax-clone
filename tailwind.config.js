d1e0698a680a61c0d8c871429d1929aec000cde7
Nicolas Moraes <nicolas.moraeslsilva@gmail.com>
2023-04-28 13:22:26 -0300

    First commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -7,12 +9,28 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'dark-blue': '#030328',
+        'violet-500': '#8529DE',
+        'violet-400': '#9E84E3',
+        'violet-200': '#9482FF',
+        'pink-500': '#FF00E5',
+      },
+      fontFamily: {
+        sans: ['var(--font-poppins)', ...fontFamily.sans],
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'secondary-button-gradient':
+          'linear-gradient(345deg, #490cb0 0%, #9b34ef 33%, rgba(255, 255, 255, 0) 66%, rgba(255, 255, 255, 0) 100% )',
+      },
+      backgroundSize: {
+        'secondary-button-size': '300% 100%',
+      },
+      backgroundPosition: {
+        'secondary-button-position': '100% 0',
+        'secondary-button-hover-position': '0',
       },
     },
   },
   plugins: [],
-}
+};
